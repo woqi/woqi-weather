c1928973d2ce049db879f2491718e1a8ff1e2eb8
woqi <741644017@qq.com>
2018-08-22 15:21:15 +0800

    add

---
+++ README.md
@@ -0,0 +1,17 @@
+####这是一个天气工具
+
+安装
+
+`npm install -g woqi-weather`
+
+在命令行输入
+
+`weather`
+
+将会显示城市天气，及未来几天的天气预报
+
+由于api原因定位城市不准确
+
+安装效果如下：
+
+<img src="https://upload-images.jianshu.io/upload_images/6194732-ae5790551b3ae2a3.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/646">
---
+++ index.js
@@ -0,0 +1,54 @@
+
+// var apiName = "http://api.jirengu.com/getWeather.php"
+// var http = require('http')
+
+// http.get(apiName, (res) => {
+//   const { statusCode } = res;
+//   const contentType = res.headers['content-type'];
+
+//   let error;
+//   if (statusCode !== 200) {
+//     error = new Error('Request Failed.\n' +
+//                       `Status Code: ${statusCode}`);
+//   } else if (!/^application\/json/.test(contentType)) {
+//     error = new Error('Invalid content-type.\n' +
+//                       `Expected application/json but received ${contentType}`);
+//   }
+
+//   res.setEncoding('utf8');
+//   let rawData = '';
+//   res.on('data', (chunk) => { rawData += chunk; });
+//   res.on('end', () => {
+//     try {
+//       const parsedData = JSON.parse(rawData);
+//       //console.log(parsedData);
+//       console.log(JSON.stringify(parsedData['results'][0]));
+//     } catch (e) {
+//       console.error(e.message);
+//     }
+//   });
+// }).on('error', (e) => {
+//   console.error(`Got error: ${e.message}`);
+// });
+
+
+var get = require('http').get;
+var weatherApi = "http://api.jirengu.com/getWeather.php";
+// var options = {
+ //   hostname: 'api.jirengu.com',
+ //   port: 80,
+ //   path: '/weather.php',
+ //   method: 'GET'
+// };
+console.log('天气信息获取中---------\n');
+var req = get(weatherApi,function(res) {
+  res.on('data', (chunk) => {
+     console.log(`BODY: ${chunk}`);
+  });
+   res.on('end', () => {
+     console.log(' 没有更多数据响应 \n No more data in response.');
+   });
+})
+.on('error', (e) => {
+ console.log(`problem with request: ${e.message}`);
+});	
